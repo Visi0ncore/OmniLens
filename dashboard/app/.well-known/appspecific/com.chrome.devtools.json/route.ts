9d578254f59624a0f88911fd66969a031af9ce60
Christopher Zeuch <chriszeuch.cz@gmail.com>
2025-09-13 22:57:21 +0200

    chore: deleted dashboard

--- dashboard/app/.well-known/appspecific/com.chrome.devtools.json/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  // Return an empty JSON object to satisfy Chrome DevTools
-  return NextResponse.json({}, { status: 200 });
-}
